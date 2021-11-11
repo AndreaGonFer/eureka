58b3d637992d41d9d80fcb3af5e82cb31900f643
Andrea <andreagf91@gmail.com>
2021-11-11 13:42:07 +0100

    Modificación archivo App.java y README.md

--- App.java
+++ App.java
@@ -1,9 +1,9 @@
 public class App {
   
   public static void main(String[] args) {
-    System.out.println("Contenido 2a");
-    System.out.println("Contenido 3");
-    System.out.println("Contenido 2b");
+    System.out.println("Andrea González");
+    System.out.println("Trobajo del Camino");
+    System.out.println("11/11/2021");
   }
   
 }
